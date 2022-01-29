a4ba23a490a43664f0dbe2fb62a519423418639d
Octavio Martinez <omartinez@logoscorp.com>
2022-01-29 02:10:39 -0400

    component maps

---
+++ components/MapBoard.vue
@@ -0,0 +1,52 @@
+<template>
+    <gmap-map
+        :center="center"
+        :zoom="zoom"
+        map-type-id="terrain"
+        :style="'width: '+width+'px; height: '+height+'px'"
+        >
+        <gmap-marker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+        />
+    </gmap-map>
+  
+</template>
+
+<script>
+module.exports = {
+    props:{
+        width: {
+            type:Number,
+            require:false,
+            default:400
+        },
+        height: {
+            type:Number,
+            require:false,
+            default:400
+        },
+        zoom: {
+            type: Number,
+            require: false,
+            default: 7
+        },
+        center: {
+            type: Object,
+            require:true
+        },
+        markers: {
+            type: Array,
+            require: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,46 @@
 (function(){
+    Vue.use(VueGoogleMaps,{
+        load:{
+            key: 'AIzaSyCzsAdLpGYkTujvr_cT1wlOQqEfrrwF2Pw',
+            libraries: 'places'
+        }
+    });
     new Vue({
         el: "#app",
         vuetify: new Vuetify(),
         components: {
           //'location': window.httpVueLoader('./components/Location.vue'),
+          'map-board': window.httpVueLoader('./components/MapBoard.vue')
         },
         data(){
             return {
                 name: "Race Map",
-                version: "1.0.0"
+                version: "1.0.0",
+                center: {
+                    lat:0, 
+                    lng:0
+                },
+                markers: [
+                    
+                ]
+            }
+        },
+        created(){
+            if ("geolocation" in navigator) {
+                console.log("geolocation")
+                navigator.geolocation.getCurrentPosition( position => {
+                    console.log(position)
+                    this.center.lat = position.coords.latitude
+                    this.center.lng = position.coords.longitude
+                    this.markers.push( {
+                        position:{
+                            lat: this.center.lat,
+                            lng: this.center.lng
+                        }
+                    } )
+                })
+            } else {
+                console.log("no geolocation")
             }
         },
         methods:{
