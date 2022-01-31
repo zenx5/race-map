c8e898c09942b6f251be88049678df5cfad9be0f
moises <elmoises.reydereymail.com>
2022-01-31 18:36:41 -0400

    agregados r2d

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,13 @@ window.getRandomReal = ( min, max ) => {
 window.distanceBetweenPoints = ( {x1,y1}, {x2,y2} ) => {
     return Math.sqrt( (x2-x1)**2 + (y2-y1)**2 );
 }
+
+function deg2rad( value ) {
+	let Pi = Math.PI;
+	return value*Pi/180;
+}
+
+function rad2deg( value ) {
+	let Pi = Math.PI;
+	return value*180/Pi;
+}
